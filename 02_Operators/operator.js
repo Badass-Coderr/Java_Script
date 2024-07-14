2be9505fb8a669b00a5f85121e430b643cf19ba5
Krishna <krishkrishnamurari@gmail.com>
2024-07-14 23:36:28 +0530

    Completed Day 2 Assignment

---
+++ 02_Operators/operator.js
@@ -0,0 +1,47 @@
+// Activity 1: Arithmetic Operations
+// Task 1-5: Write a program to add, subtract, multiply, divide, remainder of two numbers
+// and log the result to the console.
+let add = 2 + 31;
+let sub = 78 - 45;
+let mul = 4 * 7.4;
+let div = 50 / 3;
+let rem = 67 / 5;
+console.log("2 + 31 = " + add);
+console.log("78 - 45 = " + sub);
+console.log("4 * 7.4 = " + mul);
+console.log("50 / 3 = " + div);
+console.log("67 / 5 = " + rem);
+
+
+// Activity 2: Assignment Operators
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console
+add += 5.6;
+sub -= 10.7;
+console.log();
+console.log(add);       //38.6
+console.log(sub);       //22.3
+
+
+// Activity 3: Comparison Operators
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console
+console.log();
+console.log(5 > 6);         //false
+console.log(5 < 6);         //true
+console.log(5 >= 5);        //true
+console.log(5 >= 6);        //false
+console.log(5 == 5.0);      //true
+console.log(5 === 5.0);     //true
+
+
+// Activity 4: Logical Operators
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+// Task 13: White a program that uses the ! operator to negate a condition and log the result to the console.
+console.log(5 > 6 && 5 < 6);        //false
+console.log(5 > 6 || 5 < 6);        //true
+console.log(!(5 > 6) && 5 < 6);     //true
+
+
